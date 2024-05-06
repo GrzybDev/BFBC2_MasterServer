898c67ded7fa9edd785d250f2153224204b5c2ce
Marek Grzyb <marek@grzyb.dev>
2024-05-06 15:04:16 +0200

    Added ClientLocale enumerator

---
+++ bfbc2_masterserver/enumerators/ClientLocale.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class ClientLocale(Enum):
+    English = "en_US"
+    French = "fr_FR"
+    German = "de"
+    Spanish = "es"
+    Italian = "it"
+    Japanese = "ja"
+    Russian = "ru"
+    Polish = "pl"
