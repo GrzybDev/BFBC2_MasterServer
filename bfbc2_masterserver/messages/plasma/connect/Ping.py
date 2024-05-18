45e7aa9f3454ca6bb9e4d23c647094333c099d7f
Marek Grzyb <marek@grzyb.dev>
2024-05-18 17:12:53 +0200

    Added Ping message models

---
+++ bfbc2_masterserver/messages/plasma/connect/Ping.py
@@ -0,0 +1,13 @@
+from bfbc2_masterserver.messages.plasma.PlasmaTransaction import PlasmaTransaction
+
+
+class PingRequest(PlasmaTransaction):
+    """
+    This class represents a Ping message.
+    """
+
+
+class PingResponse(PlasmaTransaction):
+    """
+    This class represents a Ping response message.
+    """
