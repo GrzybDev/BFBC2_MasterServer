42c6da4712571a672d785a457294c95d26000f2b
Marek Grzyb <marek@grzyb.dev>
2024-07-06 18:55:08 +0200

    Added Records document schema

---
+++ bfbc2_masterserver/database/mongo/documents/Records.py
@@ -0,0 +1,17 @@
+from mongoengine import (
+    DateTimeField,
+    Document,
+    EnumField,
+    FloatField,
+    IntField,
+)
+
+from bfbc2_masterserver.messages.plasma.record.GetRecord import RecordName
+
+
+class Records(Document):
+    persona_id = IntField(required=True)
+    name = EnumField(RecordName)
+    key = IntField(required=True)
+    value = FloatField(required=True)
+    updated = DateTimeField(required=True)
