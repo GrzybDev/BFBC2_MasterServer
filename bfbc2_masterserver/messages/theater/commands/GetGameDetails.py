26e0a257f184969687af169f305a0118d0981807
Marek Grzyb <marek@grzyb.dev>
2024-07-21 20:08:36 +0200

    Added GetGameList theater command models

---
+++ bfbc2_masterserver/messages/theater/commands/GetGameDetails.py
@@ -0,0 +1,9 @@
+from bfbc2_masterserver.messages.theater.TheaterMessage import TheaterMessage
+
+
+class GetGameDetailsRequest(TheaterMessage):
+    pass
+
+
+class GetGameDetailsResponse(TheaterMessage):
+    pass
