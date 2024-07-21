7c32a74ce8784d902d6e9438887815af27c5ab38
Marek Grzyb <marek@grzyb.dev>
2024-07-21 20:08:13 +0200

    Added RankedStat related models

--- bfbc2_masterserver/messages/Stats.py
+++ bfbc2_masterserver/messages/Stats.py
@@ -4,3 +4,15 @@ from pydantic import BaseModel
 class Stat(BaseModel):
     key: str
     value: float
+
+
+class RankedStat(BaseModel):
+    key: str
+    value: float
+    rank: int
+
+
+class RankedStatReturn(BaseModel):
+    rankedStats: list[RankedStat]
+    ownerId: int
+    ownerType: int
