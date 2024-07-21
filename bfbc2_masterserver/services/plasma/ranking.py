135852d5d56015e5e437194dbbba11826305d18a
Marek Grzyb <marek@grzyb.dev>
2024-07-21 20:10:37 +0200

    Added support for more ranking related transactions

--- bfbc2_masterserver/services/plasma/ranking.py
+++ bfbc2_masterserver/services/plasma/ranking.py
@@ -1,8 +1,21 @@
 from bfbc2_masterserver.enumerators.Transaction import Transaction
+from bfbc2_masterserver.messages.plasma.ranking.GetRankedStatsForOwners import (
+    GetRankedStatsForOwnersRequest,
+    GetRankedStatsForOwnersResponse,
+)
+from bfbc2_masterserver.messages.plasma.ranking.GetRankedStatsRequest import (
+    GetRankedStatsRequest,
+    GetRankedStatsResponse,
+)
 from bfbc2_masterserver.messages.plasma.ranking.GetStats import (
     GetStatsRequest,
     GetStatsResponse,
 )
+from bfbc2_masterserver.messages.plasma.ranking.GetTopNAndStats import (
+    GetTopNAndStatsRequest,
+    GetTopNAndStatsResponse,
+)
+from bfbc2_masterserver.messages.Stats import RankedStat, RankedStatReturn
 from bfbc2_masterserver.services.service import Service
 
 
@@ -14,6 +27,21 @@ class RankingService(Service):
         self.resolvers[Transaction.GetStats] = (
             self.__handle_get_stats,
             GetStatsRequest,
+        )
+
+        self.resolvers[Transaction.GetRankedStatsForOwners] = (
+            self.__handle_get_ranked_stats_for_owners,
+            GetRankedStatsForOwnersRequest,
+        )
+
+        self.resolvers[Transaction.GetRankedStats] = (
+            self.__handle_get_ranked_stats,
+            GetRankedStatsRequest,
+        )
+
+        self.resolvers[Transaction.GetTopNAndStats] = (
+            self.__handle_get_top_n_and_stats,
+            GetTopNAndStatsRequest,
         )
 
     def _get_resolver(self, txn):
@@ -43,3 +71,29 @@ class RankingService(Service):
     def __handle_get_stats(self, data: GetStatsRequest):
         stats = self.database.get_stats(self.plasma.userId, data.keys)
         return GetStatsResponse(stats=stats)
+
+    def __handle_get_ranked_stats_for_owners(
+        self, data: GetRankedStatsForOwnersRequest
+    ):
+        stats: list[RankedStatReturn] = []
+
+        for owner in data.owners:
+            ownerStats = self.database.get_ranked_stats(owner.ownerId, data.keys)
+
+            stats.append(
+                RankedStatReturn(
+                    rankedStats=ownerStats,
+                    ownerId=owner.ownerId,
+                    ownerType=owner.ownerType,
+                )
+            )
+
+        return GetRankedStatsForOwnersResponse(rankedStats=stats)
+
+    def __handle_get_ranked_stats(self, data: GetRankedStatsRequest):
+        stats = self.database.get_ranked_stats(self.plasma.userId, data.keys)
+        return GetRankedStatsResponse(stats=stats)
+
+    def __handle_get_top_n_and_stats(self, data: GetTopNAndStatsRequest):
+        leaderboard = self.database.get_leaderboard(data.keys)
+        return GetTopNAndStatsResponse(stats=leaderboard)
