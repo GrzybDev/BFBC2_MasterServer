e238429ecd675fd279d35b6fc659ae4410322339
Marek Grzyb <marek@grzyb.dev>
2024-07-21 20:10:48 +0200

    Updated mongo driver

--- bfbc2_masterserver/database/mongo/mongo.py
+++ bfbc2_masterserver/database/mongo/mongo.py
@@ -1,7 +1,10 @@
+import queue
+import secrets
 from datetime import datetime
 from typing import Tuple
 
 import bcrypt
+import mongoengine
 from mongoengine import Q, connect
 from pydantic import SecretStr
 from redis import Redis
@@ -9,7 +12,9 @@ from redis import Redis
 from bfbc2_masterserver.database.database import BaseDatabase
 from bfbc2_masterserver.database.mongo.documents.Accounts import Accounts
 from bfbc2_masterserver.database.mongo.documents.Entitlements import Entitlements
+from bfbc2_masterserver.database.mongo.documents.Games import Games
 from bfbc2_masterserver.database.mongo.documents.Keys import Keys
+from bfbc2_masterserver.database.mongo.documents.Lobbies import Lobbies
 from bfbc2_masterserver.database.mongo.documents.Messages import Messages
 from bfbc2_masterserver.database.mongo.documents.Personas import Personas
 from bfbc2_masterserver.database.mongo.documents.Records import Records
@@ -17,12 +22,18 @@ from bfbc2_masterserver.database.mongo.documents.Stats import Stats as StatsDocu
 from bfbc2_masterserver.enumerators.ErrorCode import ErrorCode
 from bfbc2_masterserver.enumerators.plasma.AssocationType import AssocationType
 from bfbc2_masterserver.messages.Account import Account
+from bfbc2_masterserver.messages.Game import GameServer
 from bfbc2_masterserver.messages.Message import Attachment, Message, Target
 from bfbc2_masterserver.messages.Persona import Persona
 from bfbc2_masterserver.messages.plasma.Association import Association
 from bfbc2_masterserver.messages.plasma.Entitlement import Entitlement
+from bfbc2_masterserver.messages.plasma.ranking.GetTopNAndStats import Leaderboard
 from bfbc2_masterserver.messages.Record import Record
-from bfbc2_masterserver.messages.Stats import Stat
+from bfbc2_masterserver.messages.Stats import RankedStat, Stat
+from bfbc2_masterserver.messages.theater.commands.CreateGame import CreateGameRequest
+from bfbc2_masterserver.messages.theater.commands.GetGameList import GameData
+from bfbc2_masterserver.messages.theater.commands.GetLobbyList import Lobby
+from bfbc2_masterserver.messages.theater.commands.UpdateGame import UpdateGameRequest
 
 
 class MongoDB(BaseDatabase):
@@ -416,10 +427,87 @@ class MongoDB(BaseDatabase):
         stats = []
 
         for key in keys:
-            stat = Stat(key=key, value=statsDict.get(key, 0))
+            if key not in statsDict:
+                continue
+
+            stat = Stat(key=key, value=statsDict[key])
             stats.append(stat)
 
         return stats
+
+    def get_ranked_stats(self, persona_id, keys) -> list[RankedStat]:
+        statsDoc = StatsDocument._get_collection()
+
+        stats = []
+
+        for key in keys:
+            pipeline = [
+                {
+                    "$setWindowFields": {
+                        "sortBy": {key: 1},
+                        "output": {
+                            "rank": {"$rank": {}},
+                        },
+                    }
+                },
+                {
+                    "$match": {
+                        "persona_id": persona_id,
+                    }
+                },
+            ]
+
+            response = statsDoc.aggregate(pipeline)
+            response = dict(response)
+
+            if key not in response:
+                continue
+
+            stat = RankedStat(
+                key=key,
+                value=response[key],
+                rank=response["rank"],
+            )
+
+            stats.append(stat)
+
+        return stats
+
+    def get_leaderboard(self, keys) -> list[Leaderboard]:
+        statsDoc = StatsDocument._get_collection()
+
+        leaderboards = []
+
+        for key in keys:
+            pipeline = [
+                {
+                    "$setWindowFields": {
+                        "sortBy": {key: 1},
+                        "output": {
+                            "rank": {"$rank": {}},
+                        },
+                    }
+                },
+            ]
+
+            response = statsDoc.aggregate(pipeline)
+            response = dict(response)
+
+            if key not in response:
+                continue
+
+            leaderboard = Leaderboard(
+                addStats=[
+                    Stat(
+                        key=key,
+                        value=response[key],
+                    )
+                ]
+            )
+
+            leaderboards.append(leaderboard)
+
+        return leaderboards
 
     def get_records(self, persona_id, type) -> list[Record]:
         recordsDoc = Records.objects(persona_id=persona_id, name=type).all()
@@ -434,3 +522,211 @@ class MongoDB(BaseDatabase):
         ]
 
         return records
+
+    def get_lobbies(self) -> list[Lobby]:
+        lobbiesDoc = Lobbies.objects().all()
+
+        lobbies = [
+            Lobby.model_validate(
+                {
+                    "LID": lobby.id,
+                    "PASSING": self.get_lobby_games_count(lobby.id),
+                    "NAME": lobby.name,
+                    "LOCALE": lobby.locale,
+                    "MAX-GAMES": lobby.maxGames,
+                    "NUM-GAMES": self.get_lobby_games_count(lobby.id),
+                }
+            )
+            for lobby in lobbiesDoc
+        ]
+        return lobbies
+
+    def _add_lobby(self, name, locale) -> bool | ErrorCode:
+        if Lobbies.objects(name=name).first():
+            return False
+
+        seq_lobby = self.redis.incr("seq:lobby")
+
+        Lobbies(
+            id=seq_lobby,
+            name=name,
+            locale=locale,
+        ).save()
+
+        return True
+
+    def get_lobby_games_count(self, lobby_id) -> int:
+        gamesDoc = Games.objects(lid=lobby_id).count()
+        return gamesDoc
+
+    def get_lobby_games(self, lobby_id) -> list[GameData]:
+        gamesDoc = Games.objects(lid=lobby_id).all()
+
+        games = []
+
+        for game in gamesDoc:
+            games.append(
+                GameData.model_validate(
+                    {
+                        "LID": game.lid,
+                        "GID": game.id,
+                        "N": game.name,
+                        "AP": game.activePlayers,
+                        "JP": game.joiningPlayers,
+                        "QP": game.queuedPlayers,
+                        "MP": game.maxPlayers,
+                        "F": 0,  # Is Player Favorite
+                        "NF": 0,  # Favorite Player Count
+                        "HU": 1,
+                        "HN": "bfbc2.server.p",
+                        "I": game.addrIp,
+                        "P": game.addrPort,
+                        "J": game.joinMode,
+                        "PL": game.platform,
+                        "PW": int(0),
+                        "V": "2.0",
+                        "TYPE": game.gameType,
+                        "B-numObservers": game.numObservers,
+                        "B-maxObservers": game.maxObservers,
+                        "B-version": game.serverVersion,
+                        "B-U-region": game.gameRegion,
+                        "B-U-level": game.gameLevel,
+                        "B-U-elo": game.gameElo,
+                        "B-U-Softcore": int(game.serverSoftcore),
+                        "B-U-Hardcore": int(game.serverHardcore),
+                        "B-U-EA": int(game.serverEA),
+                        "B-U-HasPassword": int(game.serverHasPassword),
+                        "B-U-public": int(game.gamePublic),
+                        "B-U-QueueLength": game.queueLength,
+                        "B-U-gameMod": game.gameMod,
+                        "B-U-gamemode": game.gameMode,
+                        "B-U-sguid": game.gameSGUID,
+                        "B-U-Provider": game.providerId,
+                        "B-U-Time": game.gameTime,
+                        "B-U-hash": game.gameHash,
+                        "B-U-Punkbuster": int(game.serverPunkbuster),
+                        "B-U-PunkBusterVersion": None,
+                    }
+                )
+            )
+
+        return games
+
+    def create_game(self, request: CreateGameRequest) -> GameServer:
+        # If there's a game with UGID, just return that game if the secret matches
+        existingGame = Games.objects(ugid=request.UGID).first()  # type: ignore
+
+        if existingGame:
+            if existingGame.secret == request.SECRET.get_secret_value():
+                # Refresh ekey
+                existingGame.ekey = secrets.token_urlsafe(16)
+                existingGame.joinMode = request.JOIN
+                existingGame.save()
+
+                return GameServer.model_validate(
+                    {
+                        "LID": existingGame.lid,
+                        "GID": existingGame.id,
+                        "MAX-PLAYERS": existingGame.maxPlayers,
+                        "EKEY": existingGame.ekey,
+                        "UGID": existingGame.ugid,
+                        "JOIN": existingGame.joinMode,
+                        "SECRET": existingGame.secret,
+                        "J": existingGame.joinMode,
+                    }
+                )
+
+        # Create a new game
+        seq_game = self.redis.incr("seq:game")
+
+        game = Games(
+            id=seq_game,
+            lid=1,
+            name=request.NAME.lstrip('"').rstrip('"'),
+            addrIp=str(request.INT_IP),
+            addrPort=request.INT_PORT,
+            platform="PC",
+            gameType=request.TYPE,
+            queueLength=request.QLEN,
+            maxPlayers=request.MAX_PLAYERS,
+            maxObservers=request.B_maxObservers,
+            numObservers=request.B_numObservers,
+            serverHardcore=request.B_U_Hardcore,
+            serverHasPassword=request.B_U_HasPassword,
+            serverPunkbuster=request.B_U_Punkbuster,
+            clientVersion="1.0",
+            serverVersion=request.B_version,
+            joinMode=request.JOIN,
+            ugid=request.UGID,
+            ekey=secrets.token_urlsafe(16),
+            secret=request.SECRET.get_secret_value(),
+        ).save()
+
+        return GameServer.model_validate(
+            {
+                "LID": game.lid,
+                "GID": game.id,
+                "MAX-PLAYERS": game.maxPlayers,
+                "EKEY": game.ekey,
+                "UGID": game.ugid,
+                "JOIN": game.joinMode,
+                "SECRET": game.secret,
+                "J": game.joinMode,
+            }
+        )
+
+    def update_game(self, request: UpdateGameRequest) -> bool:
+        mapping = {
+            "N": "name",
+            "AP": "activePlayers",
+            "JP": "joiningPlayers",
+            "QP": "queuedPlayers",
+            "MP": "maxPlayers",
+            "F": "0",
+            "NF": "0",
+            "HU": "numObservers",
+            "HN": "name",
+            "I": "addrIp",
+            "P": "addrPort",
+            "J": "joinMode",
+            "PL": "platform",
+            "PW": "serverHasPassword",
+            "V": "serverVersion",
+            "TYPE": "gameType",
+            "B-numObservers": "numObservers",
+            "B-maxObservers": "maxObservers",
+            "B-version": "serverVersion",
+            "B-U-region": "gameRegion",
+            "B-U-level": "gameLevel",
+            "B-U-elo": "gameElo",
+            "B-U-Softcore": "serverSoftcore",
+            "B-U-Hardcore": "serverHardcore",
+            "B-U-EA": "serverEA",
+            "B-U-HasPassword": "serverHasPassword",
+            "B-U-public": "gamePublic",
+            "B-U-QueueLength": "queueLength",
+            "B-U-gameMod": "gameMod",
+            "B-U-gamemode": "gameMode",
+            "B-U-sguid": "gameSGUID",
+            "B-U-Provider": "providerId",
+            "B-U-Time": "gameTime",
+            "B-U-hash": "gameHash",
+            "B-U-Punkbuster": "serverPunkbuster",
+            "B-U-PunkBusterVersion": "punkbusterVersion",
+        }
+
+        gid = request.GID
+
+        game = Games.objects(id=gid).first()
+
+        if not game:
+            return False
+
+        updated_data = request.model_dump()
+
+        for key, value in updated_data.items():
+            setattr(game, mapping.get(key, key), value)
+
+        game.save()
+
+        return True
