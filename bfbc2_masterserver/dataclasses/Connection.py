6fd0e1a2930c69b3fae6a6a4298d086056016398
Marek Grzyb <marek@grzyb.dev>
2024-08-04 18:31:54 +0200

    Updated BaseConnection dataclass

--- bfbc2_masterserver/dataclasses/Connection.py
+++ bfbc2_masterserver/dataclasses/Connection.py
@@ -1,16 +1,21 @@
 from bfbc2_masterserver.enumerators.client.ClientLocale import ClientLocale
+from bfbc2_masterserver.enumerators.client.ClientPlatform import ClientPlatform
 from bfbc2_masterserver.enumerators.client.ClientType import ClientType
 
 
 class BaseConnection:
     clientName: str
     locale: ClientLocale
+    platform: ClientPlatform
     type: ClientType
     fragmentSize: int
 
     accountId: int
     accountLoginKey: str
+    personaName: str
     personaId: int
     personaLoginKey: str
+    gameId: int
+
     internalIp: str
     internalPort: int
